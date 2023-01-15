e21bfb7ba4ee68dece1d3d4b6a97e6a20c975923
adarijani <ali.darijani.me@gmail.com>
2023-01-15 23:44:52 +0100

    Update make_resume.sh

--- LaTeX_templates/short_resume/make_resume.sh
+++ LaTeX_templates/short_resume/make_resume.sh
@@ -2,4 +2,4 @@
 
 lualatex short_resume.tex
 convert short_resume.pdf short_resume.jpg
-convert short_resume.jpg -resize 50%  short_resume_50.jpg
+convert short_resume.jpg -resize 30%  short_resume_50.jpg
