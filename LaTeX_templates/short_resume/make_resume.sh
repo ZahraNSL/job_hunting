04afd08a3abd3d684204f2324962605c7909a479
adarijani <ali.darijani.me@gmail.com>
2023-01-21 10:11:04 +0100

    commit

--- LaTeX_templates/short_resume/make_resume.sh
+++ LaTeX_templates/short_resume/make_resume.sh
@@ -4,3 +4,6 @@ git pull
 lualatex short_resume.tex
 convert short_resume.pdf short_resume.jpg
 convert short_resume.jpg -resize 30%  short_resume_30.jpg
+git add .
+git commit -a -m "commit" 
+git push
